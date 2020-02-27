dc82de26cc40aff119e56b7a84c49acba320c2aa
Vid <vid.repar@gmail.com>
2020-02-27 12:43:15 +0100

    feat: Add exchange rates to the page

--- src/ExchangeRates.tsx
+++ src/ExchangeRates.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
+import { spawn } from "child_process";
 
 export default function ExchangeRates() {
   const { loading, error, data } = useQuery(RATES_QUERY);
 
-  return <>{JSON.stringify(data)}</>;
+  console.log(data);
+
+  return (
+    <>
+      {data && // TODO: remove if loading === true
+        data.rates.map((rate: any) => (
+          <div key={rate.currency}>
+            {rate.currency} | {rate.rate}
+          </div>
+        ))}
+    </>
+  );
 }
 
 const RATES_QUERY = gql`
