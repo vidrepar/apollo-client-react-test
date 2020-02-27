7a897c274135e4683bc61bf0fba04115b83d8acb
Vid <vid.repar@gmail.com>
2020-02-27 12:59:54 +0100

    feat: Add authorization header

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,5 +1,13 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { authLink } from "./Authorization";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const httpLink = createHttpLink({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
 
 export const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io"
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
